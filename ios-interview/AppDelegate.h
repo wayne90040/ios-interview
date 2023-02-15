9b8bb94d0ab84e702b5c084412d27f860660842f
Wayne Hsu <wayne8_hsu@asjadetech.com>
2023-02-15 12:39:12 +0800

    Initial Commit

---
+++ ios-interview/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ios-interview
+//
+//  Created by WEI LUN HSU on 2023/2/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
